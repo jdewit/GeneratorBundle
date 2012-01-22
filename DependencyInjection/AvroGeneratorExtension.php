4b0ac23d866d502f326dbe6d1a9e063a791493ce
Joris de Wit <joris.w.dewit@gmail.com>
2012-01-22 12:16:19 -0800

    update

--- DependencyInjection/AvroGeneratorExtension.php
+++ DependencyInjection/AvroGeneratorExtension.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Avro\GeneratorBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -7,11 +6,12 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-class AvroGeneratorExtension extends Extension {
-
+class AvroGeneratorExtension extends Extension 
+{
     public function load(array $configs, ContainerBuilder $container) {
+        
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('config.yml');
     }
 
 }
+
--- Skeleton/Bundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace {{ bundle_namespace }};
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class {{ bundle_name }} extends Bundle
-{
-}
