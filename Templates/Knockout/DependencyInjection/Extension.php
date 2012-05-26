677c83c1e2344a2b60b535ba20664f86255127f4
Joris de Wit <joris.w.dewit@gmail.com>
2012-05-26 09:42:55 -0700

    add missing use statement

--- Templates/Knockout/DependencyInjection/Extension.php
+++ Templates/Knockout/DependencyInjection/Extension.php
@@ -4,6 +4,7 @@ namespace {{ bundleNamespace }}\DependencyInjection;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 
 class {{ bundleVendor }}{{ bundleCoreName | ucFirst }}Extension extends Extension 
