ae9060d50e9fa8f2668de5aca184b6e7bdd923a0
Joris de Wit <joris.w.dewit@gmail.com>
2012-09-10 12:36:18 -0700

    add interface generator

---
+++ Templates/Mongo/Model/Interface.php
@@ -0,0 +1,33 @@
+<?php
+namespace {{ bundleNamespace }}\Model;
+
+/**
+ *
+ * @author Joris de Wit <joris.w.dewit@gmail.com>
+ */
+interface {{ entity }}Interface
+{
+{% for field in fields %}
+{% set adjustedFieldName = field.fieldName|slice(0, -1) %}
+{% if field.type == "many" %}
+    public function get{{ field.fieldName | ucFirst }}();
+
+    public function set{{ field.fieldName | ucFirst }}(\{{ field.targetDocument }} ${{ field.fieldName }} = null);
+
+    public function add{{ adjustedFieldName|ucFirst }}(\{{ field.targetDocument }} ${{ adjustedFieldName }});
+
+    public function remove{{ field.adjustedFieldName | ucFirst }}(\{{ field.targetDocument }} ${{ field.fieldName }} = null);
+
+{% elseif field.type == "one" %}
+    public function get{{ field.fieldName|ucFirst }}();
+
+    public function set{{ field.fieldName | ucFirst }}(\{{ field.targetDocument }} ${{ field.fieldName }} = null);
+
+{% else %}
+    public function get{{ field.fieldName|ucFirst }}();
+
+    public function set{{ field.fieldName|ucFirst }}(${{ field.fieldName }});
+
+{% endif %}
+{% endfor %}
+}
